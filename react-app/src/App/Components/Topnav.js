1cd086518dfca699445ea14add45cde8821e4d4e
joel chin <joelczq1993@gmail.com>
2021-06-14 00:31:11 +0800

    clean up topnav after including react material ui compatible

--- react-app/src/App/Components/Topnav.js
+++ react-app/src/App/Components/Topnav.js
@@ -4,23 +4,8 @@ import "./Topnav.css";
 import { Button } from '@material-ui/core';
 import { ButtonTheme } from "../Themes/theme";
 
-// class Topnav extends React.Component {
-
-//     render() {
-//         return (
-//             <div className="topnav">
-//                 <Button id="home" className="base-item" href="/">Home</Button>
-//                 <Button id="track" className="base-item" href="/track">Track</Button>
-//                 <Button id="contact" className="base-item" href="/">Contact</Button>
-//             </div>
-//         )
-//     }
-// }
-
-// export default Topnav;
 
 const useStyles = ButtonTheme
-
 
 export default function Topnav() {
     const classes = useStyles();
