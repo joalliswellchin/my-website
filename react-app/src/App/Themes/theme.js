a4aa3d16fc8ecaf7c825bd33240ee2a870953f6d
joel chin <joelczq1993@gmail.com>
2021-06-14 01:04:47 +0800

    add animation gradient on header button

--- react-app/src/App/Themes/theme.js
+++ react-app/src/App/Themes/theme.js
@@ -13,9 +13,22 @@ export const ButtonTheme = makeStyles((theme) => (
             color: '#bdbcbc',
             padding: theme.spacing(1),
             '&:hover': {
-                background:theme.palette.secondary,
+                background:'linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab)',
                 color: '#676666',
+                backgroundSize: '400% 400%',
+                animation: `$fadingGradient 5s ${theme.transitions.easing.easeInOut} infinite`,
             },
         },
+        '@keyframes fadingGradient': {
+            '0%': {
+                backgroundPosition: '0% 50%',
+            },
+            '50%': {
+                backgroundPosition: '100% 50%',
+            },
+            '100%': {
+                backgroundPosition: '0% 50%',
+            },
+        }
     }
 ));
