294126ef355409c8c2ec95ba8e1606cfdf534f44
joel chin <joelczq1993@gmail.com>
2021-06-15 01:25:44 +0800

    add contact and icons

--- react-app/src/App/Pages/Contact.js
+++ react-app/src/App/Pages/Contact.js
@@ -1,13 +1,50 @@
 import Header from "../Components/Header";
 import "./Contact.css";
+
+// Getting icons from material ui
+// https://material-ui.com/components/material-icons/
 import InstagramIcon from '@material-ui/icons/Instagram';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import EmailIcon from '@material-ui/icons/Email';
 
 const Contact = () => {
     return (
         <div className="Contact">
-            <Header/>
-            <h1>You are on Contact</h1>
-            <InstagramIcon/>
+            <div className="nav">
+                <Header />
+            </div>
+            <div className="caption1">
+                You can contact me through the following modes below!
+            </div>
+            <div className="icon">
+                <a href="/contact">
+                    <span className="insta" title="coming soon!">
+                        <InstagramIcon />
+                    </span>
+                </a>
+                <a href="/contact">
+                    <span className="fb" title="coming soon!">
+                        <FacebookIcon />
+                    </span>
+                </a>
+                <a href="https://github.com/joalliswellchin">
+                    <span className="github">
+                        <GitHubIcon />
+                    </span>
+                </a>
+                <a href="https://www.linkedin.com/in/joel-chin-a4a129139/">
+                    <span className="linkedin">
+                        <LinkedInIcon />
+                    </span>
+                </a>
+                <a href="/contact">
+                    <span className="email" title="coming soon!">
+                        <EmailIcon />
+                    </span>
+                </a>
+            </div>
         </div>
     )
 }
